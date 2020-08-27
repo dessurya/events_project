398c60e9018cf2f0c987a1ba56d990b7f254e709
Adam Surya Des <fourline66@gmail.com>
2020-08-27 08:35:50 +0700

    Update dtables_script.blade.php

--- resources/views/panel/_componen/dtables_script.blade.php
+++ resources/views/panel/_componen/dtables_script.blade.php
@@ -3,4 +3,4 @@
     const componenTable = jQuery.parseJSON('{!! json_encode($config['componen']) !!}');
     const rendorTableBodyOn = '#{!! $config['table_id'] !!} tbody';
 </script>
-<script type="text/javascript" src="{{ asset('asset/js/dtable.js') }}"></script>
+<script type="text/javascript" src="{{ asset('asset/js/dtable.js') }}?v=0.1"></script>
