e8ca139571965fc25e9746722f01c023c2660644
Adam Surya Des <fourline66@gmail.com>
2020-09-17 13:04:11 +0700

    Update 2020_09_17_093206_update_event_coupon_view.php

--- database/migrations/2020_09_17_093206_update_event_coupon_view.php
+++ database/migrations/2020_09_17_093206_update_event_coupon_view.php
@@ -74,6 +74,74 @@ class UpdateEventCouponView extends Migration
             LEFT JOIN master_status_self ps ON ps.self_id = ecr.status and ps.parent_id = 4
         )
         ");
+
+        DB::statement("CREATE OR REPLACE VIEW `v_history_event` AS 
+            SELECT
+                a.id, 
+                a.title, 
+                1 as event_id,
+                'Tournament TO' as event,
+                a.start_registration, 
+                a.end_registration, 
+                a.start_activity as start_event,
+                a.end_activity as end_event,
+                status_id,
+                status,
+                registration_status_id,
+                registration_status_name,
+                description,
+                terms_and_conditions,
+                picture,
+                b.created_at
+            FROM
+                v_event_tournament_to a
+            LEFT JOIN
+                event_tournament_to b on a.id = b.id
+            UNION ALL
+            SELECT
+                a.id, 
+                a.title, 
+                2 as event_id,
+                'Coupon' as event,
+                a.start_registration, 
+                a.end_registration, 
+                a.start_active as start_event,
+                a.end_active as end_event,
+                status_id,
+                status,
+                registration_status_id,
+                registration_status as registration_status_name,
+                description,
+                terms_and_conditions,
+                picture,
+                b.created_at
+            FROM
+                v_event_coupon a
+            LEFT JOIN
+                event_coupon b on a.id = b.id
+            UNION ALL
+            SELECT
+                a.id, 
+                a.title, 
+                3 as event_id,
+                'Other Event' as event,
+                '' as start_registration, 
+                '' as end_registration, 
+                a.start_activity as start_event,
+                a.end_activity as end_event,
+                status_id,
+                status,
+                2 as registration_status_id,
+                'Deny' as registration_status_name,
+                description,
+                terms_and_conditions,
+                picture,
+                b.created_at
+            FROM
+                v_event_other a
+            LEFT JOIN
+                event_other b on a.id = b.id
+        ");
     }
 
     /**
@@ -135,6 +203,74 @@ class UpdateEventCouponView extends Migration
             LEFT JOIN master_status_self es ON es.self_id = ec.flag_status and es.parent_id = 1
             LEFT JOIN master_status_self ps ON ps.self_id = ecr.status and ps.parent_id = 4
         )
+        ");
+
+        DB::statement("CREATE OR REPLACE VIEW `v_history_event` AS 
+            SELECT
+                a.id, 
+                a.title, 
+                1 as event_id,
+                'Tournament TO' as event,
+                a.start_registration, 
+                a.end_registration, 
+                a.start_activity as start_event,
+                a.end_activity as end_event,
+                status_id,
+                status,
+                registration_status_id,
+                registration_status_name,
+                description,
+                terms_and_conditions,
+                picture,
+                b.created_at
+            FROM
+                v_event_tournament_to a
+            LEFT JOIN
+                event_tournament_to b on a.id = b.id
+            UNION ALL
+            SELECT
+                a.id, 
+                a.title, 
+                2 as event_id,
+                'Coupon' as event,
+                a.start_registration, 
+                a.end_registration, 
+                a.start_active as start_event,
+                a.end_active as end_event,
+                status_id,
+                status,
+                registration_status_id,
+                registration_status_name,
+                description,
+                terms_and_conditions,
+                picture,
+                b.created_at
+            FROM
+                v_event_coupon a
+            LEFT JOIN
+                event_coupon b on a.id = b.id
+            UNION ALL
+            SELECT
+                a.id, 
+                a.title, 
+                3 as event_id,
+                'Other Event' as event,
+                '' as start_registration, 
+                '' as end_registration, 
+                a.start_activity as start_event,
+                a.end_activity as end_event,
+                status_id,
+                status,
+                2 as registration_status_id,
+                'Deny' as registration_status_name,
+                description,
+                terms_and_conditions,
+                picture,
+                b.created_at
+            FROM
+                v_event_other a
+            LEFT JOIN
+                event_other b on a.id = b.id
         ");
     }
 }
