8cc786e56d93b45e0a3e6a61190b86fd83213fdd
Adam Surya Des <fourline66@gmail.com>
2020-10-02 13:30:36 +0700

    Update GenerateNumberPeriodeRecord.php

--- app/Console/Commands/GenerateNumberPeriodeRecord.php
+++ app/Console/Commands/GenerateNumberPeriodeRecord.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class GenerateNumberPeriodeRecord extends Command
 {
@@ -39,18 +40,22 @@ class GenerateNumberPeriodeRecord extends Command
      */
     public function handle()
     {
-        $setting = json_decode(Cache::get('generate_number_setting'),true);
-        $getTimeOfPeriode = $setting['periode'];
-        $cache = json_decode(Cache::get('generate_number_result'),true);
-        $expairedPeriod = strtotime(date('Y-m-d H:i:s', strtotime('+'.$getTimeOfPeriode.' minutes', $cache['live_time'])));
-        $now = strtotime(now());
-        if ($expairedPeriod <= $now) {
-            DB::table('history_generate_number')->insert([
-                'periode_from' => date('Y-m-d H:i:s', $cache['live_time']),
-                'periode_to' => date('Y-m-d H:i:s', $expairedPeriod),
-                'resault' => json_encode($cache)
-            ]);
+        Log::notice('generatenumber:record || running');
+        if (Cache::has('generate_number_setting') and Cache::has('generate_number_result')) {
+            $setting = json_decode(Cache::get('generate_number_setting'),true);
+            $getTimeOfPeriode = $setting['periode'];
+            $cache = json_decode(Cache::get('generate_number_result'),true);
+            $expairedPeriod = strtotime(date('Y-m-d H:i:s', strtotime('+'.$getTimeOfPeriode.' minutes', $cache['live_time'])));
+            $now = strtotime(now());
+            if ($expairedPeriod <= $now) {
+                DB::table('history_generate_number')->insert([
+                    'periode_from' => date('Y-m-d H:i:s', $cache['live_time']),
+                    'periode_to' => date('Y-m-d H:i:s', $expairedPeriod),
+                    'resault' => json_encode($cache)
+                ]);
+                Log::notice('generatenumber:record || store periode and cleared');
+                Cache::forget('generate_number_result');
+            }
         }
-        Cache::forget('generate_number_result');
     }
 }
